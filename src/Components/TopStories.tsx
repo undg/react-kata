9d0371ee0e5f5d25854c3df93d235ba6bdd5b48b
und3rdg <und3rdg@gmail.com>
2021-04-07 23:22:23 +0100

    Lazy styling with material ui.

--- src/Components/TopStories.tsx
+++ src/Components/TopStories.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { LoadingComponent } from '.'
 import { HackerNewsItem } from '../api'
 import { TopStoriesContext } from '../context'
+import { Button, Card, CardActions, CardContent, Typography } from '@material-ui/core'
 
 export const TopStoriesComponent: React.FC = () => {
     const ctx = useContext(TopStoriesContext)
@@ -18,11 +19,25 @@ export const TopStoriesComponent: React.FC = () => {
 export const TopStoriesItem: React.FC<HackerNewsItem> = (props) => {
     return (
         <>
-            <h3 className="stories__item__title">{props.title}</h3>
-            <p className="stories__item__author">by: {props.by}</p>
-            <div className="stories__item__date">{new Date(props.time).toDateString()}</div>
-            <a className="stories__item__link" href={props.url}>Link</a>
-            <hr />
+            <Card className="stories__item" variant="outlined">
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2" className="stories__item__title">
+                        {props.title}
+                    </Typography>
+                    <Typography variant="body2" component="p" color="primary" className="stories__item__author">
+                        by: {props.by}
+                    </Typography>
+                    <Typography variant="body2" component="p" color="textSecondary" className="stories__item__date">
+                        {new Date(props.time).toDateString()}
+                    </Typography>
+                </CardContent>
+
+                <CardActions>
+                    <Button variant="contained" size="small" className="stories__item__link" href={props.url}>
+                        Link
+                    </Button>
+                </CardActions>
+            </Card>
         </>
     )
 }
